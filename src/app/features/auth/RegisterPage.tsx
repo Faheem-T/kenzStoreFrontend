0bbcfd5fcfd9be74a65a2f0d663dc4c5bd66886a
Faheem-T <faheemmbasheer@gmail.com>
2024-12-30 12:09:07 +0530

    complete register page

--- src/app/features/auth/RegisterPage.tsx
+++ src/app/features/auth/RegisterPage.tsx
@@ -1,64 +1,129 @@
-import { TextField, Box, Button, Stack, Typography } from "@mui/material"
+import { TextField, Box, Button, Stack, Typography, Snackbar, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton, FormHelperText } from "@mui/material"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { DevTool } from "@hookform/devtools"
 import { z } from "zod"
 import { useRegisterMutation } from "../../api"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+import { Link } from "react-router"
 
 const registerSchema = z.object({
-    /* firstName: z.string().nonempty("First name is required").min(3, "Name cannot be less than 3 characters"), */
-    firstName: z.string(),
-    lastName: z.string().nonempty("Last name is required"),
+    firstName: z.string().nonempty("First name is required").min(3, "Name cannot be less than 3 characters"),
     email: z.string().nonempty("Email is required").email("Email format is not valid"),
-    DOB: z.date(),
-    password: z.string().nonempty("Password is required")
+    password: z.string().nonempty("Password is required"),
+    confirmPassword: z.string().nonempty("Confirmation is required")
 })
 
 export type registerFormValues = {
     firstName: string;
-    lastName: string;
-    email: string;
-    DOB: Date;
     password: string;
+    confirmPassword: string;
+    email: string;
 }
-
 export const RegisterPage = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    /* const [serverIssues, setServerIssues] = useState([]) */
+
+    /* useEffect(() => {
+*     if (serverIssues) {
+*         serverIssues.forEach((issue) => {
+*             toast.error(issue)
+*         })
+*     }
+* }, [serverIssues])
+ */
     const form = useForm<registerFormValues>({
         defaultValues: {
             firstName: "",
-            lastName: "",
             email: "",
-            DOB: new Date(),
             password: "",
-
+            confirmPassword: "",
         }, resolver: zodResolver(registerSchema)
     });
 
-    const { register, handleSubmit, control, formState } = form;
+    const { register, handleSubmit, control, formState, setError } = form;
 
     const { errors } = formState;
 
     const [createRegisterMutation, { isLoading }] = useRegisterMutation();
 
-    const submitHandler = (data: registerFormValues) => {
-        console.log(data)
-        createRegisterMutation(data)
+
+    const submitHandler = async (data: registerFormValues) => {
+        if (data.confirmPassword !== data.password) {
+            setError("confirmPassword", { message: "Confirmation does not match", type: "value" })
+            return;
+        }
+
+        try {
+            await createRegisterMutation(data).unwrap()
+        } catch (error: any) {
+            console.log(error)
+            /* setServerIssues(error.data.issues.map((issue: { message: string, field: string }) => issue.message)) */
+            error.data.issues.forEach((issue: { message: string, field: "firstName" | "password" | "confirmPassword" | "email" }) => {
+                setError(issue.field, { message: issue.message, type: "value" })
+            })
+        }
     }
 
+
     return (
-        <Box sx={{ height: '100vh', display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-            <Typography margin={4} variant="h2" fontWeight={800}>Register</Typography>
-            <form noValidate onSubmit={handleSubmit(submitHandler)}>
-                <Stack spacing={2} paddingY={0} paddingX={50}>
-                    <TextField label="First Name" {...register("firstName")} error={!!errors.firstName} helperText={errors.firstName?.message} />
-                    <TextField label="Last Name" {...register("lastName")} error={!!errors.lastName} helperText={errors.lastName?.message} />
-                    <TextField label="Email" type="email" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
-                    <TextField label="DOB" type="date" {...register("DOB", { valueAsDate: true })} error={!!errors.DOB} helperText={errors.DOB?.message} />
-                    <TextField label="Password" type="password" {...register("password")} error={!!errors.password} helperText={errors.password?.message} />
-                    <Button type="submit" variant="contained" color="primary" disabled={isLoading}>{isLoading ? "Loading..." : "Register"}</Button>
-                </Stack>
-                <DevTool control={control} />
-            </form>
-        </Box>
+        <>
+            <Box sx={{ height: '100vh', display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", gap: "8px" }}>
+                <Typography margin={4} variant="h2" fontWeight={800}>Register</Typography>
+                <form noValidate onSubmit={handleSubmit(submitHandler)}>
+                    <Stack spacing={2} paddingY={0} paddingX={8}>
+                        <TextField label="First Name" {...register("firstName")} error={!!errors.firstName} helperText={errors.firstName?.message} />
+                        <TextField label="Email" type="email" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
+                        {/* <TextField label="Password" type="password" {...register("password")} error={!!errors.password} helperText={errors.password?.message} /> */}
+                        <FormControl variant="outlined" >
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <OutlinedInput
+                                id="password"
+                                {...register("password")}
+                                type={showPassword ? "text" : "password"}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={() => setShowPassword((state) => !state)}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                error={!!errors.password}
+                            />
+                            {!!errors.password &&
+                                <FormHelperText error>{errors.password?.message}</FormHelperText>
+                            }
+                        </FormControl>
+                        <FormControl variant="outlined" >
+                            <InputLabel>Confirm Password</InputLabel>
+                            <OutlinedInput
+                                {...register("confirmPassword")}
+                                type={showPassword ? "text" : "password"}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={() => setShowPassword((state) => !state)}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                error={!!errors.confirmPassword}
+                            />
+                            {!!errors.confirmPassword &&
+                                <FormHelperText error>{errors.confirmPassword?.message}</FormHelperText>
+                            }
+                        </FormControl>
+                        <Button type="submit" variant="contained" color="primary" disabled={isLoading}>{isLoading ? "Loading..." : "Register"}</Button>
+                    </Stack>
+                    <DevTool control={control} />
+                </form>
+                <Typography variant="caption">Already registered? <Link to="/login">LOGIN</Link></Typography>
+            </Box>
+        </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { RegisterPage } from "./app/features/auth/RegisterPage.tsx";
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+import { Toaster } from "react-hot-toast";
 const router = createBrowserRouter([
     {
         path: ""
@@ -20,14 +21,31 @@ const router = createBrowserRouter([
 ]
 )
 
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <RouterProvider router={router} />
-            {/* <App /> */}
-        </ThemeProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Toaster
+                    position="bottom-left"
+                    toastOptions={{
+                        className: '',
+                        style: {
+                            border: '',
+                            padding: '8px',
+                            color: theme.palette.text.primary,
+                            backgroundColor: theme.palette.secondary.main,
+                            fontFamily: theme.typography.fontFamily,
+                            textTransform: "uppercase",
+                            fontWeight: "500",
+                            fontSize: "12px"
+                        },
+                    }}
+                />
+                <RouterProvider router={router} />
+                {/* <App /> */}
+            </ThemeProvider>
         </Provider>
     </StrictMode>
 );
