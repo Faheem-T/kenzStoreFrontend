35fef17f73f71451e9c66a0bd49239666bd92d80
Faheem-T <faheemmbasheer@gmail.com>
2025-01-21 12:13:00 +0530

    Add discount display component

---
+++ src/app/components/ProductDiscountedPriceDisplay.tsx
@@ -0,0 +1,40 @@
+import { Box, Typography } from "@mui/material";
+
+export const DiscountedPriceDisplay = ({
+  price,
+  finalPrice,
+  isDiscountActive,
+  smallFont = false,
+}: {
+  price: number;
+  finalPrice: number;
+  isDiscountActive: boolean;
+  smallFont?: boolean;
+}) => {
+  const fontSize = smallFont ? "1em" : "1.5em";
+  return (
+    <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+      <Typography
+        // variant={isDiscountActive ? "h6" : "h5"}
+        sx={{
+          fontWeight: "bold",
+          //   fontSize: isDiscountActive ? "1em" : "1.5em",
+          fontSize: isDiscountActive ? ".9em" : fontSize,
+          textDecoration: isDiscountActive ? "line-through" : "inital",
+        }}
+        color={isDiscountActive ? "textDisabled" : "textPrimary"}
+      >
+        QR {price}/-
+      </Typography>
+      {isDiscountActive && (
+        <Typography
+          //   variant="h5"
+          color="accent.main"
+          sx={{ fontWeight: "bold", fontSize }}
+        >
+          QR {finalPrice}/-
+        </Typography>
+      )}
+    </Box>
+  );
+};
