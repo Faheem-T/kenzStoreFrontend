a893d14bcc9ea23ff3440631713c9e142e0026cb
Faheem-T <faheemmbasheer@gmail.com>
2025-03-04 17:49:44 +0530

    tweak loading component

--- src/app/components/LoadingComponent.tsx
+++ src/app/components/LoadingComponent.tsx
@@ -6,10 +6,12 @@ interface LoadingComponentTypes {
 
 const LoadingComponent = ({ fullScreen }: LoadingComponentTypes) => {
   const height = fullScreen ? "100vh" : "100%";
+  const width = fullScreen ? "100vw" : "100%";
   return (
     <Box
       sx={{
         height,
+        width,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
